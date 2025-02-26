f10a3f1bef0b2e2d0d03cb25dc70b9332e2139f4
hex-aragon <0xaragon90@gmail.com>
2025-02-26 23:15:37 +0900

    EDIT: server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 const newSvc = require("./services/newsSvc");
 const lib = require("./util/lib");
 const path = require("path");
